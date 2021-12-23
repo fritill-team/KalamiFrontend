db6dce1ad1c8cf0fc6f2161cf2e427bb65987787
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-12-23 15:55:03 +0200

    layout - edit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import '@/styles/index.scss'
 import 'muicss/dist/js/mui.min.js'
 import '@/js/sidebar'
+import '@/js/toggle-header'
---
+++ src/js/toggle-header.js
@@ -0,0 +1,7 @@
+import 'muicss/dist/js/mui.min.js'
+
+$(document).on('click', '.pack-card-container', function () {
+  console.log('clicked');
+  // $("#header").after('<div>').addClass("expand-header");
+  $("#header").after('<div class="expand-header"></div>');
+})
