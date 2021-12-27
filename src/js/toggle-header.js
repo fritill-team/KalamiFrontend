78969467ab0d6f366acd8556d8b7ac6d3ab491a7
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-12-27 18:18:01 +0200

    edit

--- src/js/toggle-header.js
+++ src/js/toggle-header.js
@@ -2,8 +2,22 @@
 
 $(document).on('click', '.pack-card', function () {
   console.log('clicked');
+  let self = $(this);
+  let img = self.find('img').attr('src')
+  let bgcolor = self.find('.pack-card').attr('background-color')
   // $("#header").after('<div>').addClass("expand-header");
-  $("header").after(`<div class="expand-header">
-    <img src='../images/girl.png' />
-  </div>`);
+  $("header").after($(`
+  <div class="expand-header" style='background-color': ${bgcolor}>
+    <div>
+      <i class="material-icons">add</i>
+    </div>
+    <img src='${img}' alt="card image" />
+    <div class="expand-header__description">
+      <div class="expand-header__titl">sister</div>
+      <div class="expand-header__subtitle">family</div>
+      <span class="material-icons">
+        volume_up
+      </span>
+    </div>
+  </div>`));
 })
