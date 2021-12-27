aaba7965ebcb5ad09f65d66153e2b23d37b8e3ef
sohype.mop@gmail.com <sohype.mop@gmail.com>
2021-12-27 18:37:46 +0200

    retreive auth

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import 'material-design-lite/material.min'
 import '@/js/sidebar'
 import '@/js/toggle-header'
 import '@/js/packCard'
-// window.flip = function(flip) {
-//   $('#cube').removeClass();
-//   $('#cube').addClass(flip);
-// }
+
+window.flip = function(flip) {
+  $('#cube').removeClass();
+  $('#cube').addClass(flip);
+}
