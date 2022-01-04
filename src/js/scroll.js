36e2cec8797eebce1be5957e2c18ac6ca88e8c03
MohammedIbraheemZaki <zakiz0054@gmail.com>
2022-01-04 18:11:42 +0200

    1- finish home

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -3,26 +3,18 @@ $(function () { // wait for document ready
   $(document).on('scroll', function(){
     console.log('scrolling');
   })
-  // var elem = document.querySelector(".content");
-  
-  // var scrollbar = Scrollbar.init(elem)
-
-  // scrollbar.addListener(() => {
-  //   scene.refresh()
-  // })
-
 
   var controller = new ScrollMagic.Controller();
 
-  // var tween = TweenMax()
-    // .fromTo("section.panel.blue", 1, {width: '50%', float: 'right'})
-
-  // define movement of panels
   var wipeAnimation = new TimelineMax()
     .fromTo("section.panel.turqoise", 1, {x: "0%"}, {x: "100%", ease: Linear.easeAll})
+    .fromTo("#screen1", 1, {opacity: 1}, {opacity: 0})
     .fromTo("section.panel.blue", 1, {x: "0%"}, {x: "100%", ease: Linear.easeAll})
+    .fromTo("#screen2", 1, {opacity: 0}, {opacity: 1})
     .fromTo("section.panel.green", 1, {x:  "-100%"}, {x: "-0%", ease: Linear.easeAll})
+    .fromTo("#screen2", 1, {opacity: 1}, {opacity: 0})
     .fromTo("section.panel.green", 1, {x:  "0%"}, {x: "-100%", ease: Linear.easeAll})
+    .fromTo("#screen3", 1, {opacity: 0}, {opacity: 1})
     .fromTo("section.panel.blue", 1, {x: "100%"}, {x: "0%", ease: Linear.easeAll})
     .fromTo("section.panel.purple", 1, {x: "100%"}, {x: "0%", ease: Linear.easeAll})
     
@@ -37,28 +29,3 @@ $(function () { // wait for document ready
     .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
 });
-
-// $(function(){
-//   var tl = new TimelineMax({onUpdate: updatePercentage});
-
-//   const controller = new ScrollMagic.Controller();
-
-//   tl.from("#first", 0.5, {x: '100%', opacity: 0})
-//   // tl.from('#first', 0.5, {x: '100%'})
-//   // tl.from('#second', 0.5, {x: '0%'})
-
-//   const scene = new ScrollMagic.Scene({
-//     triggerElement: '.hero',
-//     triggerHook: 'onLeave',
-//     duration: '100%',
-//     trigger: true
-//   })
-//   .setPin('.hero')
-//   .setTween(tl)
-//   .addTo(controller)
-
-//   function updatePercentage(){
-//     tl.progress();
-//     console.log(tl.progress());
-//   }
-// })
